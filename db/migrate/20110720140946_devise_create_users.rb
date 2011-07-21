3da55927d97f94d36bafd97bf1aab2af51942f66
throwern <throwern@cb9d5a27-2d10-0410-b862-890ba643f4ea>
2011-07-21 20:13:45 +0000

    add login

--- db/migrate/20110720140946_devise_create_users.rb
+++ db/migrate/20110720140946_devise_create_users.rb
@@ -2,7 +2,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
       t.database_authenticatable :null => false
-      # t.string :login, :null => false, :unique => true
+      t.string :login, :null => false, :unique => true
       # t.recoverable
       t.rememberable
       t.trackable
@@ -16,7 +16,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
     add_index :users, :login, :unique => true
-    # add_index :users, :email,                :unique => true
+    add_index :users, :email,                :unique => true
     # add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
