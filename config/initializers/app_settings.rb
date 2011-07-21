401e462e139d543b95a8eb2501e038f75dd1424c
throwern <throwern@cb9d5a27-2d10-0410-b862-890ba643f4ea>
2011-07-21 19:22:19 +0000

    initial import

---
+++ config/initializers/app_settings.rb
@@ -0,0 +1,4 @@
+APP_CONFIG = {
+  :use_remote_user => false,
+  :create_from_remote_user => true
+}
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GenomeSuite::Application.config.secret_token = '6792df39b5999b0fdf88f17a57a0c1bc04d411d16d60105c3c2b3a38091ed51b0bd23ee185a116b5fe034f150a9970afabc136b8fd370603a81f6d748df1e499'
