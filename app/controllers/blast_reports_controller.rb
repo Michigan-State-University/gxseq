cb86f8d4556d3e7f2defe695dbd17679a00cd530
throwern <throwern@cb9d5a27-2d10-0410-b862-890ba643f4ea>
2013-03-11 22:51:43 +0000

    update blast report alignment view

---
+++ app/controllers/blast_reports_controller.rb
@@ -0,0 +1,18 @@
+class BlastReportsController < ApplicationController
+
+  before_filter :find_blast_report, :only => [:show, :alignment, :edit]
+  
+  def alignment
+    @hit = @report.hits.find{|hit| hit.accession == params[:hit_accession]}
+    respond_to do |format|
+      format.html {render :partial => 'alignment', :locals => {:hit => @hit, :report => @report}}
+      format.js {}
+    end
+  end
+  
+  def find_blast_report
+    @blast_report = BlastReport.find(params[:id])
+    @blast_run = @blast_report.blast_run
+    @report = @blast_report.report
+  end
+end
