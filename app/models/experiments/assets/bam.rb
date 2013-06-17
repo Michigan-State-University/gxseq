49b7cd036581be325b1c6f9758fa390c6961bc72
throwern <throwern@cb9d5a27-2d10-0410-b862-890ba643f4ea>
2013-06-17 17:48:42 +0000

    new read count function

--- app/models/experiments/assets/bam.rb
+++ app/models/experiments/assets/bam.rb
@@ -30,8 +30,7 @@ class Bam < Asset
     end
   end
   
-  # NOTE does NOT work with delayed_job background tasks
-  # TODO: Test background task again, document results and reasons
+  # NOTE The samtools gem forks a child a redirects stdout. This does NOT work with delayed_job background tasks
   # returns hash of index data
   # { "sequence_1"  => {:length => 1000, :unmapped_reads => 10, :mapped_reads => 1000}, ... }
   def index_stats
@@ -56,11 +55,19 @@ class Bam < Asset
   end
   # counts the total number of mapped reads in the bam
   def total_mapped_reads
-    index_stats.inject(0){|sum,info| sum+=info[1][:mapped_reads]}
+    return [] unless bam = open_bam
+    stats = bam.flagstat
+    bam.close
+    return stats[:n_mapped][0]+stats[:n_mapped][1]
   end
   # counts the total number of un-mapped reads in the bam
   def total_unmapped_reads
-    index_stats.inject(0){|sum,info| sum+=info[1][:unmapped_reads]}
+    return [] unless bam = open_bam
+    stats = bam.flagstat
+    bam.close
+    total_reads = stats[:n_reads][0]+stats[:n_reads][1]
+    total_mapped = stats[:n_mapped][0]+stats[:n_mapped][1]
+    return total_reads - total_mapped
   end
   # returns reads overlapping the supplied region
   def get_reads(left,right,seq)
