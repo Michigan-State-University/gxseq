417a89f4d53fd509b46f0238cdb738d4601e45fa
Matt Larson <wakko978@gmail.com>
2013-09-30 10:44:43 -0400

    FIX: Migration fix

--- db/migrate/20130924172430_transfer_serialized_blast_reports.rb
+++ db/migrate/20130924172430_transfer_serialized_blast_reports.rb
@@ -1,8 +1,8 @@
 class TransferSerializedBlastReports < ActiveRecord::Migration
   def self.up
-    BlastRun.all.each do |blast_run|
-      BlastReport.transaction do
-        blast_run.blast_reports.select('seqfeature_id,report').find_in_batches(:batch_size => 500) do |batch|
+    BlastReport.transaction do
+      BlastRun.all.each do |blast_run|
+        blast_run.blast_reports.select('id,seqfeature_id,report').find_in_batches(:batch_size => 500) do |batch|
           batch.each do |blast_report|
             blast_report.report.iterations.each do |iteration|
               blast_run.populate_blast_iteration(iteration,blast_report.seqfeature_id)
